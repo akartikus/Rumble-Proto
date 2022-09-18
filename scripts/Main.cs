cef9566037f4629eea2b36e0e11397c21defb260
akartikus <sitraka.razafimamy@gmail.com>
2022-09-18 17:30:35 +0200

    Init repo

---
+++ scripts/Main.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class Main : Node2D
+{
+    private Vector2 _initialPosition = new Vector2(256, 230);
+
+    public override void _Ready()
+    {
+        var player = GetNode<Player>("Player");
+        this.StartFight(player);
+    }
+
+    public void PlayerFallDown(Player player)
+    {
+        GD.Print("On exit from Main: " + player.Name);
+        player.KillMe();
+    }
+
+    public void StartFight(Player player)
+    {
+        player.Position = _initialPosition;
+        // TODO Some fight initialisations
+    }
+}
